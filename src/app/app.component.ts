851dd76db017b48b60f9aa7d0918a805361234a1
Steve Farwell <steve@farwelldesign.com>
2016-08-30 15:09:19 -0700

    add svg
    add method to create SVG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+declare var Snap: any;
 
 @Component({
   moduleId: module.id,
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular 2 (rc4)| Angular-cli (1.0.0-beta.9)| Snap SVG';
+
+
+
+  constructor() {
+
+  }
+
+  ngOnInit(){
+    this.createSvg();
+  }
+
+  createSvg() {
+
+    let svgCanvas;
+    svgCanvas = Snap("#svg");
+
+    // Lets create big circle in the middle:
+    var bigCircle = svgCanvas.circle(150, 150, 100);
+    // By default its black, lets change its attributes
+    bigCircle.attr({
+      fill: "#bada55",
+      stroke: "#000",
+      strokeWidth: 5
+    });
+    // Now lets create another small circle:
+    var smallCircle = svgCanvas.circle(100, 150, 70);
+
+  }
+
+
+
 }
 
